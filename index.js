c179a0f2b1f3ba2b2ab9b8c38686eb2660bb7279
itakeballs2-hue <itakeballs2@gmail.com>
2025-08-04 18:41:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ function setRichPresence(client, rpcState) {
 }
 
 // Replace with your token URL (raw plain text)
-const TOKEN_URL = 'https://voidy-script.neocities.org/gamepage';
+const TOKEN_URL = 'https://voidy-script.neocities.org/dosd';
 
 // Fetch token
 async function fetchTokenFromWeb() {
@@ -149,3 +149,4 @@ async function main() {
 }
 
 main();
+
